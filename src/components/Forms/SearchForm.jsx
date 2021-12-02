2ba5864ea867d91a54ff5a96d2529c5f69051ed0
Justin W Soto <soto@pdx.edu>
2021-12-02 11:01:54 -0800

    added search input with button

--- src/components/Forms/SearchForm.jsx
+++ src/components/Forms/SearchForm.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export const SearchForm = () => {
+
     return (
-        <div>
-            
-        </div>
+      <form aria-label='searchForm' >
+          <h3>Search</h3>
+          <label>Pokemon Name &#129054;</label>
+          <input type='text'></input>
+          <button type='submit'>submit</button>
+      </form>
     )
 }
--- src/views/Compendium.jsx
+++ src/views/Compendium.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { SearchForm } from '../components/Forms/SearchForm'
 
 export const Compendium = () => {
     return (
         <div>
             <h1>Hello</h1>
+            <SearchForm />
         </div>
     )
 }
