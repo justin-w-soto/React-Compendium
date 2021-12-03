c937ccc22b7f602da627a2e37282bc2fa9d746c5
Justin W Soto <soto@pdx.edu>
2021-12-03 11:05:23 -0800

    replaced input

--- src/components/Forms/SearchForm.jsx
+++ src/components/Forms/SearchForm.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const SearchForm = () => {
   return (
     <div>
-      
+      <label>search for your favorite Pokemon</label>
+      <input type='text' />
+      <button type='submit'>submit</button>
     </div>
   )
 }
