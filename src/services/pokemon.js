8d5915b25e776f7f0b2399a5bab26a410e075a9a
Justin W Soto <soto@pdx.edu>
2021-12-03 11:20:25 -0800

    added useEffect hook for fetching pokemon

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,4 +1,33 @@
-export default function pokemon(params) {
+
+const favPokemon = [
+    'charizard',
+    'typhlosion',
+    'blaziken',
+    'mewtwo',
+    'suicune',
+    'pidgeot',
+    'golduck',
+    'gardevoir',
+    'ninetales',
+    'houndoom',
+  ];
+  
+export const fetchPokemon = async () => {
+    const pokemonList = await Promise.all(
+        favPokemon.map(async (fav) => {
+          const fetchedPokemon = await fetch(
+            `https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${fav}`
+          );
     
-};
+          const pokemonData = await fetchedPokemon.json();
+    
+          
+         
+           
+        })
+      );
+      return pokemonList;
+    };
+    
+
 
--- src/views/Compendium.jsx
+++ src/views/Compendium.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { FilterForm } from '../components/Forms/FilterForm';
 import { SearchForm } from '../components/Forms/SearchForm';
 import { PokemonList } from '../components/PokemonList/PokemonList';
+import { fetchPokemon } from '../services/pokemon';
+
 import pokeball from '../assets/pokeball.png';
 import '../App.css';
 
 export const Compendium = () => {
+    const [loading, setLoading] = useState(true);
+  const [pokemons, setPokemons] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  const [types, setTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState('all');
+
+  
+  useEffect(() => {
+    const getPokemon = async () => {
+      const pokemonList = await fetchPokemon();
+       setPokemons(pokemonList);
+      setLoading(false);
+    };
+
+    getPokemon();
+  }, []);
+
     return (
         <div>
             <img className= 'pokeball' src={pokeball} alt='pokeball' />
